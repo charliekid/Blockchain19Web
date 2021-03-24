b4e400a724271d6f609def5cc0db97845c052da6
JL47 <jorlopez@csumb.edu>
2021-03-23 18:05:33 -0700

    Registration template created

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var dashboardRouter = require('./routes/dashboard');
+var registrationRouter = require('./routes/registration')
 
 var app = express();
 
@@ -36,7 +37,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/dashboard', dashboardRouter);
-
+app.use('/registration',registrationRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
