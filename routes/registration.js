9c6604c9e788c2023afe5a356d698eafa89021a8
JL47 <jorlopez@csumb.edu>
2021-03-24 14:21:37 -0700

    Fixed registration template + changed style

--- routes/registration.js
+++ routes/registration.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 router.get('/', function(req,res,next){
     res.render('registration');
-})
+});
+
+
+router.post('/',function(reg,res,next){
+
+});
 
 module.exports = router;
