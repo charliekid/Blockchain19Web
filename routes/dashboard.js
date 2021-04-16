02f8d0a65c2c0b5aced5fd625483474b468028c2
JL47 <jorlopez@csumb.edu>
2021-04-16 12:53:34 -0700

    changed what info each party can see

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -177,4 +177,10 @@ router.get('/employer', function(req,res,next){
     });
 
 });
+
+router.get('/logout', function(req,res,next) {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
 module.exports = router;
