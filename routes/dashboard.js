5c27f7031a5bc1d4e1c78269be0d05f0f2e6fda8
Charlie Nguyen <charliekid19@gmail.com>
2021-04-20 16:02:22 -0700

    stubbed out console logs

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -41,7 +41,7 @@ router.get('/patient', function(req, res, next) {
                 //console.log("data length = [" + data.length + "]");
                 var substring = data.substr(9, data.length);
                 substring = substring.replaceAll("@", "");
-                console.log("\nsubstring = " + substring);
+                // console.log("\nsubstring = " + substring);
                 json = JSON.parse(substring);
 
                 // checks to see if we got any data or not.
@@ -51,8 +51,8 @@ router.get('/patient', function(req, res, next) {
                         count++;
                     }
                 }
-                console.log("count :" + count);
-                console.log(json.data);
+                // console.log("count :" + count);
+                // console.log(json.data);
                 // console.log("first name is :" + json.data[0].state.data.firstName);
 
 
@@ -92,7 +92,7 @@ router.get('/doctor', function(req, res, next) {
                 //console.log("data length = [" + data.length + "]");
                 var substring = data.substr(9, data.length);
                 substring = substring.replaceAll("@", "");
-                console.log("\nsubstring = " + substring);
+                // console.log("\nsubstring = " + substring);
                 json = JSON.parse(substring);
 
                 // checks to see if we got any data or not.
@@ -102,9 +102,9 @@ router.get('/doctor', function(req, res, next) {
                         count++;
                     }
                 }
-                console.log("count :" + count);
-                console.log(json.data);
-                console.log("first name is :" + json.data[0].state.data.firstName);
+                // console.log("count :" + count);
+                // console.log(json.data);
+                // console.log("first name is :" + json.data[0].state.data.firstName);
 
 
                 res.render('dashboards/doctor-dashboard', {transactions : json.data, PartyName: req.session.username});
@@ -170,10 +170,10 @@ router.get('/clinic', function(req,res,next){
           try{
             var substring = data.substr(9, data.length);
             substring = substring.replaceAll("@", "");
-            console.log("\nsubstring = " + substring);
+            // console.log("\nsubstring = " + substring);
             json = JSON.parse(substring);
 
-            console.log(json.data);
+            // console.log(json.data);
 
             res.render('dashboards/clinic-dashboard', {transactions : json.data, PartyName: req.session.username});
           }
@@ -203,10 +203,10 @@ router.get('/employer', function(req,res,next){
           try{
             var substring = data.substr(9, data.length);
             substring = substring.replaceAll("@", "");
-            console.log("\nsubstring = " + substring);
+            // console.log("\nsubstring = " + substring);
             json = JSON.parse(substring);
 
-            console.log(json.data);
+            // console.log(json.data);
 
             res.render('dashboards/employer-dashboard', {transactions : json.data, PartyName: req.session.username});
           }
